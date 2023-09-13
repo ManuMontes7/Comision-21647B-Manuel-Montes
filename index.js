66fd513ce78562c23ecc21f6e9207a322d150a27
ManuMontes7 <99566175+ManuMontes7@users.noreply.github.com>
2023-09-13 05:45:18 -0300

    BD creada

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-const express = require('express')
+const express = require('express');
+const { sequelize } = require('./src/models');
 
 const app = express()
 const port = 3000
 
-app.listen(port, async () => {
-    console.log('Servidor corriendo en el puerto ${port}')})
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Conexión a la base de datos establecida con éxito");
+  })
+  .catch((error) => {
+    console.error("Error al conectar con la base de datos:", error);
+  });
+
+  app.get('/', (req, res) => {
+    res.send('hello world')
+  })
+
+  app.listen(port, async () => {
+    console.log('Servidor corriendo en el puerto', 3000)})
