3778117768761daec7b707cb9f53f1b27c1e73f2
ManuMontes7 <99566175+ManuMontes7@users.noreply.github.com>
2023-09-14 12:26:21 -0300

    Pag de Inicio y Boostrap

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-const express = require('express');
-const { sequelize } = require('./src/models');
-const cors = require('cors');
-const morgan = require('morgan');
-const userRoutes = require('./src/routes/user.routes.js')
-const postRoutes = require('./src/routes/post.routes.js')
+const express = require("express");
+const { sequelize } = require("./src/models");
+const cors = require("cors");
+const morgan = require("morgan");
+const userRoutes = require("./src/routes/user.routes.js");
+const postRoutes = require("./src/routes/post.routes.js");
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.set('view engine', 'ejs')
-app.set('views', __dirname + '/src/views')
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/src/views");
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(cors())
-app.use(morgan('dev'))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+app.use(morgan("dev"));
 
-app.use('/user', userRoutes)
-app.use('/post', postRoutes)
+app.use("/user", userRoutes);
+app.use("/post", postRoutes);
 
 sequelize
   .authenticate()
@@ -28,8 +28,12 @@ sequelize
     console.error("Error al conectar con la base de datos:", error);
   });
 
-  app.get('/', function(req, res) {
-    res.render('base/index');});
+app.get("/", function (req, res) {
+  res.render("base/index");
+});
 
-  app.listen(port, async () => {
-    console.log(`Servidor corriendo en el puerto ${port}`)})
+//app.use('/img',express.static("/img/"));
+
+app.listen(port, async () => {
+  console.log(`Servidor corriendo en el puerto ${port}`);
+});
